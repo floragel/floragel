d4aa3fedec9d7cebd5ad988b34a9dcd208f2edb0
floragel <floragel2024@gmail.com>
2024-12-03 20:33:28 -0500

    Update mail.js

--- mail.js
+++ mail.js
@@ -2,14 +2,14 @@ function sendMail() {
     console.log("sendMail() appelé");
 
     let name = document.getElementById("name").value;
-    let email = document.getElementById("mail").value;
+    let mail = document.getElementById("mail").value;
     let message = document.getElementById("message").value;
 
     console.log("Nom :", name);
-    console.log("Email :", email);
+    console.log("Email :", mail);
     console.log("Message :", message);
 
-    let parms = { name, email, message };
+    let parms = { name, mail, message };
     console.log("Paramètres envoyés :", parms);
 
     emailjs.send("service_lds4jxo", "template_dpd7x2s", parms)
